55c6b7d91a681f6debda774ab3772f2357cbc2c6
Sumit Paroothi <paroothisumit@gmail.com>
2016-01-19 22:12:18 +0530

    options updated

--- app/src/main/java/kids/cute/spacethello/activities/options.java
+++ app/src/main/java/kids/cute/spacethello/activities/options.java
@@ -8,44 +8,90 @@ import android.widget.AbsListView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import kids.cute.spacethello.R;
 
 public class options extends Activity {
-    int sound=1;
+
     int check=1;
+    //get values from shared preferences;set accordingly
+    boolean selectedcoin=false;
+    boolean legalmoves=false;
+    boolean sound=false;
+    Switch legalmove;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+        ToggleButton sunbtn=(ToggleButton)findViewById(R.id.sunbtn);
+        sunbtn.setChecked(true);
+    }
 
-    }
-    public void buttonclick(View view)
-    {
-        view.setSelected(!view.isSelected());
-        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
-    }
-    public void legalmoves(View checkbox)
-    {
-       // if(checkbox.isSelected())
-        check++;
-            if(check%2==0)
-                Toast.makeText(this,"Selected",Toast.LENGTH_SHORT).show();
-    }
 
     public void soundbutton(View view)
     {
-        sound++;
+        sound=!sound;
        ImageView img=(ImageView)findViewById(R.id.image);
-        if(sound%2==0)
-         img.setImageResource(R.drawable.silent);
+        if(!sound)
+        {
+            img.setImageResource(R.drawable.silent);
+
+        }
         else
             img.setImageResource(R.drawable.sound);
 
     }
+    public void sunclick(View v)
+    {   Toast.makeText(this,"Sun",Toast.LENGTH_LONG).show();
+        ToggleButton sunbtn=(ToggleButton)findViewById(R.id.sunbtn);
+        sunbtn.setChecked(true);
+        ToggleButton moonbtn=(ToggleButton)findViewById(R.id.moonbtn);
+
+        moonbtn.setChecked(false);
+        selectedcoin=false;
+    }
+    public void moonclick(View v)
+    {
+        ToggleButton moonbtn=(ToggleButton)findViewById(R.id.moonbtn);
+        moonbtn.setChecked(true);
+        ToggleButton sunbtn=(ToggleButton)findViewById(R.id.sunbtn);
+        sunbtn.setChecked(false);
+        selectedcoin=true;
+    }
+    public void switcher_legal_moves(View v)
+    {
+        legalmove=(Switch)findViewById(R.id.legalmoves);
+        if(legalmove.isChecked()) {
+            legalmoves = true;
+        }
+            else{
+            legalmoves=false;
+
+        }
+
+
+    }
+    public void applyclicked(View v)
+    {
+        Toast.makeText(this, "Settings Applied", Toast.LENGTH_SHORT).show();
+        //Update shared preferences
+        //Use intent
+    }
+
     public void back(View view)
     {
-        Toast.makeText(this, "Back", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Settings Not Saved", Toast.LENGTH_SHORT).show();
+        //Do not update shared preferences
+        //Use Intent
+    }
+    public void Resetbtn(View v)
+    {    selectedcoin=false;
+        legalmoves=false;
+         sound=false;
+
+        Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show();
     }
 }
