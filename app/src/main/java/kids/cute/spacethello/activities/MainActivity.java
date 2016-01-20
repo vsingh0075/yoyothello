5cc9edc8210d9d77999b2f03271b67b702723852
Sumit Paroothi <paroothisumit@gmail.com>
2016-01-20 21:50:26 +0530

    Shared Preferences

--- app/src/main/java/kids/cute/spacethello/activities/MainActivity.java
+++ app/src/main/java/kids/cute/spacethello/activities/MainActivity.java
@@ -11,30 +11,62 @@ import android.widget.ImageView;
 import kids.cute.spacethello.R;
 
 public class MainActivity extends Activity {
-    public int sound=0;
+    public boolean sound;
+    SharedPreferences.Editor editor;
+    SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        set_shared_preferences();
         /*
             Full Screen !!
          */
 
     }
-    public void toggle(View view){
+    public  void set_shared_preferences()
+    {
+         pref = getApplicationContext().getSharedPreferences("gamesettings", MODE_PRIVATE);
+         editor = pref.edit();
+
+        boolean sound=pref.getBoolean("sound",true);
+        editor.putBoolean("sound",sound);
+        editor.commit();
         ImageView img=(ImageView)findViewById(R.id.sound);
-        if(sound==0)
+        if(sound)
         {
             img.setImageResource(R.drawable.sound);
-            sound=1;
+            //sound=true;
+
         }
         else
         {
             img.setImageResource(R.drawable.silent);
-            sound=0;
+            //sound=false;
         }
+
+    }
+    public void toggle(View view){
+        ImageView img=(ImageView)findViewById(R.id.sound);
+        if(sound==false)
+        {
+            img.setImageResource(R.drawable.sound);
+            sound=true;
+
+        } else {
+            img.setImageResource(R.drawable.silent);
+            sound=false;
+
+        }
+        editor.putBoolean("sound",sound);
+        editor.commit();
+    }
+    public void optionsclicked(View v)
+    {
+        Intent in=new Intent(this,options.class);
+        startActivity(in);
     }
     public void SinglePlayer(View view)
     {
--- app/src/main/java/kids/cute/spacethello/activities/options.java
+++ app/src/main/java/kids/cute/spacethello/activities/options.java
@@ -1,6 +1,7 @@
 package kids.cute.spacethello.activities;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,52 +19,101 @@ public class options extends Activity {
 
     int check=1;
     //get values from shared preferences;set accordingly
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
     boolean selectedcoin=false;
     boolean legalmoves=false;
     boolean sound=false;
     Switch legalmove;
+    ImageView img;
+    ToggleButton sunbtn,moonbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
-        ToggleButton sunbtn=(ToggleButton)findViewById(R.id.sunbtn);
-        sunbtn.setChecked(true);
+
+        img=(ImageView)findViewById(R.id.image_sound);
+        legalmove=(Switch)findViewById(R.id.legalmoves);
+        sunbtn=(ToggleButton)findViewById(R.id.sunbtn);
+        moonbtn=(ToggleButton)findViewById(R.id.moonbtn);
+        get_shared_preferences();
+
+
     }
 
+    public void get_shared_preferences()
+    {
+        pref = getApplicationContext().getSharedPreferences("gamesettings", MODE_PRIVATE);
+        editor = pref.edit();
+        sound =pref.getBoolean("sound",true);
+        selectedcoin=pref.getBoolean("selectedcoin",false);
+        legalmoves=pref.getBoolean("legalmoves",true);
+        editor.putBoolean("sound",sound);
+        editor.putBoolean("legalmoves",legalmoves);
+        editor.putBoolean("selectedcoin", selectedcoin);
+        editor.commit();
+        apply_settings();
 
+
+    }
+    public void apply_settings()
+    {
+        if(sound)
+            img.setImageResource(R.drawable.sound);
+        else
+            img.setImageResource(R.drawable.silent);
+        if(legalmoves)
+            legalmove.setChecked(true);
+        else
+            legalmove.setChecked(false);
+        if(selectedcoin)
+        {
+            moonbtn.setChecked(true);
+
+            sunbtn.setChecked(false);
+        }
+        else
+        {
+            moonbtn.setChecked(false);
+
+            sunbtn.setChecked(true);
+        }
+    }
     public void soundbutton(View view)
     {
-        sound=!sound;
-       ImageView img=(ImageView)findViewById(R.id.image);
-        if(!sound)
+
+
+        if(sound)
         {
             img.setImageResource(R.drawable.silent);
 
         }
         else
+        {
             img.setImageResource(R.drawable.sound);
-
+        }
+        sound=!sound;
     }
-    public void sunclick(View v)
-    {   Toast.makeText(this,"Sun",Toast.LENGTH_LONG).show();
-        ToggleButton sunbtn=(ToggleButton)findViewById(R.id.sunbtn);
+    public void sunClick(View v)
+    {
+
         sunbtn.setChecked(true);
-        ToggleButton moonbtn=(ToggleButton)findViewById(R.id.moonbtn);
 
         moonbtn.setChecked(false);
         selectedcoin=false;
     }
-    public void moonclick(View v)
+    public void moonClick(View v)
     {
-        ToggleButton moonbtn=(ToggleButton)findViewById(R.id.moonbtn);
+
         moonbtn.setChecked(true);
-        ToggleButton sunbtn=(ToggleButton)findViewById(R.id.sunbtn);
+
         sunbtn.setChecked(false);
         selectedcoin=true;
     }
     public void switcher_legal_moves(View v)
     {
-        legalmove=(Switch)findViewById(R.id.legalmoves);
+
         if(legalmove.isChecked()) {
             legalmoves = true;
         }
@@ -73,10 +123,16 @@ public class options extends Activity {
         }
 
 
+
     }
     public void applyclicked(View v)
     {
         Toast.makeText(this, "Settings Applied", Toast.LENGTH_SHORT).show();
+        editor.putBoolean("legalmoves", legalmoves);
+        editor.putBoolean("sound", sound);
+        editor.putBoolean("selectedcoin",selectedcoin);
+        editor.commit();
+        System.exit(0);
         //Update shared preferences
         //Use intent
     }
@@ -84,13 +140,25 @@ public class options extends Activity {
     public void back(View view)
     {
         Toast.makeText(this, "Settings Not Saved", Toast.LENGTH_SHORT).show();
+        editor.clear();
+        System.exit(0);
         //Do not update shared preferences
         //Use Intent
     }
     public void Resetbtn(View v)
+
     {    selectedcoin=false;
-        legalmoves=false;
-         sound=false;
+        legalmoves=true;
+         sound=true;
+
+        img.setImageResource(R.drawable.sound);
+
+        legalmove.setChecked(true);
+
+        sunbtn.setChecked(true);
+
+
+        moonbtn.setChecked(false);
 
         Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show();
     }
